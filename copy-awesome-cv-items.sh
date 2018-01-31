ab562a9b4bff46471f79e596e93fe4cf56d2bde9
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2018-01-31 12:14:53 -0500

    renamed and update

--- copy-awesome-cv-items.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cp -r Awesome-CV/fonts .
-cp Awesome-CV/awesome-cv.cls .
---
+++ generate-build-dir.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+mkdir -p build
+cp -a Awesome-CV/fonts/ build/fonts/
+cp Awesome-CV/awesome-cv.cls build/
+cp Awesome-CV/fontawesome.sty build/
