c1b247bbdc5f1df2ad04c336fe04c639df67386d
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2018-01-31 14:58:06 -0500

    release candidate with html

---
+++ generate-site-dir.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cp -a Skeleton/css site/
--- generate_cv.py
+++ generate_cv.py
@@ -20,6 +20,15 @@ def load_latex_template(path: Path):
     return template
 
 
+def load_html_template(path: Path):
+    environment = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(str(path.parent.resolve())),
+    )
+    template = environment.get_template(path.name)
+
+    return template
+
+
 def run():
     # load data
     root_dir = Path(__file__).parent
@@ -28,19 +37,32 @@ def run():
     with open(cv_data_path) as f:
         cv_data = yaml.load(f)
 
-    # load jinja
+    # load latex template
     template_path = root_dir / 'templates' / 'cv-template.tex'  # type: Path
     logging.info(f'Loading template: {template_path.resolve()}')
     template = load_latex_template(template_path)
 
     # generate cv
     build_dir = root_dir / 'build'  # type: Path
-    build_dir.mkdir(exist_ok=True)
     cv_tex_path = build_dir / 'nicholas-nadeau-cv.tex'
     logging.info(f'Exporting tex: {cv_tex_path.resolve()}')
     with open(cv_tex_path, 'w') as f:
         out = template.render(cv=cv_data)
         f.write(out)
+
+    # load html template
+    template_path = root_dir / 'templates' / 'index-template.html'  # type: Path
+    logging.info(f'Loading template: {template_path.resolve()}')
+    template = load_html_template(template_path)
+
+    # generate cv
+    site_dir = root_dir / 'site'  # type: Path
+    index_path = site_dir / 'index.html'
+    logging.info(f'Exporting tex: {index_path.resolve()}')
+    with open(index_path, 'w') as f:
+        out = template.render(cv=cv_data)
+        f.write(out)
+
     logging.info('Generation complete')
 
 
