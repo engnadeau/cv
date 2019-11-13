1f91304485d15258bfc18c9d9481e058bb2c814f
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2019-11-13 07:45:16 -0500

    updated script

--- scripts/render_templates.py
+++ scripts/render_templates.py
@@ -2,18 +2,30 @@ import logging
 from pathlib import Path
 
 import jinja2
-import yaml
+import toml
+
+ROOT_DIR = Path(__file__).parents[1]
+TEMPLATES_DIR = ROOT_DIR / "templates"
+BUILD_DIR = ROOT_DIR / "build"
+PDF_DIR = BUILD_DIR / "pdf"
+SITE_DIR = BUILD_DIR / "site"
+
+CV_PATH = ROOT_DIR / "cv.toml"
+TEX_TEMPLATE_PATH = TEMPLATES_DIR / "template.tex"
+HTML_TEMPLATE_PATH = TEMPLATES_DIR / "template.html"
+TEX_OUTPUT_PATH = PDF_DIR / "cv.tex"
+HTML_OUTPUT_PATH = SITE_DIR / "index.html"
 
 
 def load_latex_template(path: Path):
     environment = jinja2.Environment(
         loader=jinja2.FileSystemLoader(str(path.parent.resolve())),
-        block_start_string='\B{',
-        block_end_string='}',
-        variable_start_string='\V{',
-        variable_end_string='}',
-        comment_start_string='\C{',
-        comment_end_string='}',
+        block_start_string="\B{",
+        block_end_string="}",
+        variable_start_string="\V{",
+        variable_end_string="}",
+        comment_start_string="\C{",
+        comment_end_string="}",
     )
     template = environment.get_template(path.name)
 
@@ -22,50 +34,42 @@ def load_latex_template(path: Path):
 
 def load_html_template(path: Path):
     environment = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(str(path.parent.resolve())),
+        loader=jinja2.FileSystemLoader(str(path.parent.resolve()))
     )
     template = environment.get_template(path.name)
 
     return template
 
 
-def run():
-    # load data
-    root_dir = Path(__file__).parent
-    cv_data_path = root_dir / 'cv.yaml'  # type: Path
-    logging.info(f'Loading {cv_data_path.resolve()}')
-    with open(cv_data_path) as f:
-        cv_data = yaml.load(f)
+def render_html(data: dict):
+    logging.info(f"Loading template: {HTML_TEMPLATE_PATH.relative_to(ROOT_DIR)}")
+    template = load_html_template(HTML_TEMPLATE_PATH)
 
-    # load latex template
-    template_path = root_dir / 'templates' / 'cv-template.tex'  # type: Path
-    logging.info(f'Loading template: {template_path.resolve()}')
-    template = load_latex_template(template_path)
-
-    # generate cv
-    build_dir = root_dir / 'build'  # type: Path
-    cv_tex_path = build_dir / f'nicholas-nadeau-cv.tex'
-    logging.info(f'Exporting tex: {cv_tex_path.resolve()}')
-    with open(cv_tex_path, 'w') as f:
-        out = template.render(cv=cv_data)
+    logging.info(f"Exporting: {HTML_OUTPUT_PATH.relative_to(ROOT_DIR)}")
+    with open(HTML_OUTPUT_PATH, "w") as f:
+        out = template.render(data=data)
         f.write(out)
 
-    # load html template
-    template_path = root_dir / 'templates' / 'index-template.html'  # type: Path
-    logging.info(f'Loading template: {template_path.resolve()}')
-    template = load_html_template(template_path)
 
-    # generate cv
-    site_dir = root_dir / 'site'  # type: Path
-    index_path = site_dir / 'index.html'
-    logging.info(f'Exporting tex: {index_path.resolve()}')
-    with open(index_path, 'w') as f:
-        out = template.render(cv=cv_data)
+def render_tex(data: dict):
+    logging.info(f"Loading template: {TEX_TEMPLATE_PATH.relative_to(ROOT_DIR)}")
+    template = load_latex_template(TEX_TEMPLATE_PATH)
+
+    logging.info(f"Exporting: {TEX_OUTPUT_PATH.relative_to(ROOT_DIR)}")
+    with open(TEX_OUTPUT_PATH, "w") as f:
+        out = template.render(data=data)
         f.write(out)
 
-    logging.info('Generation complete')
+
+def main():
+    logging.info(f"Loading {CV_PATH.relative_to(ROOT_DIR)}")
+    with open(CV_PATH) as f:
+        data = toml.load(f)
+
+    render_tex(data)
+    render_html(data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    run()
+    main()
